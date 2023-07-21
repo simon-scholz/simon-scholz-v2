298b9fe7f25fa72ec8b8c796f33577ccd08d0c74
Simon Scholz <simonscholz@outlook.com>
2023-07-21 16:46:52 +0200

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,161 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/JetBrains-Mono/JetBrainsMono-Regular.woff2"
+          as="font"
+          crossOrigin=""
         />
-      </div>
+        <title>Simon Scholz</title>
+        <meta name="description" content="Simon Scholz - Designer, who codes" />
+        <meta name="keywords" content="UX, UI, Product Design, Development" />
+        <meta name="author" content="Simon Scholz" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+        <script
+          async
+          data-no-cookie
+          data-respect-dnt
+          data-api="/_hive"
+          src="/bee.js"
+        ></script>
+      </Head>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <main
+        className={`bg-white flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+        <div className="flex flex-col items-start w-full max-w-4xl">
+          <h2 className="font-semibold">Hey, I'm Simon</h2>
+          <p className="text-base pt-2">
+            I design, build, and ship digital products. Right now I'm a product
+            manager and UX/UI lead at 360dialog, a global Facebook business
+            solution provider, enabling companies of all sizes and from all over
+            the world to use WhatsApp.
           </p>
-        </a>
+          <p className="text-base pt-2">
+            I've previously co-founded CDF Technologies, where I led the design
+            and creative direction of multiple B2B FinTech products and design
+            systems.
+          </p>
+          <p className="text-base pt-2">
+            Before CDF I worked as an automotive engineer, specializing in
+            thermal and mechanical load collective simulation of powertrain
+            components.
+          </p>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        <div className="flex flex-col items-start w-full max-w-4xl">
+          <p className="bg-gray-800 rounded-full px-2 py-1 text-xs text-white tracking-wide mb-6 mt-20">
+            WORK
           </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+          <div className="flex flex-row justify-start align-baseline gap-6 px-2 py-2 w-full">
+            <div className="flex flex-row justify-start align-baseline gap-8 w-full">
+              <p className="text-gray-800 font-medium min-w-fit">360dialog</p>
+              <p className="overflow-hidden text-gray-200 flex-shrink-1">
+                {".".repeat(100)}
+              </p>
+              <p className="text-gray-600 min-w-fit">
+                Product Manager & UX/UI Lead
+              </p>
+            </div>
+            <p className="text-slate-400 font-jetbrains w-52">2020 —</p>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <div className="flex flex-row justify-start align-baseline gap-6 px-2 py-2 w-full">
+            <div className="flex flex-row justify-start align-baseline gap-8 w-full">
+              <p className="text-gray-800 font-medium min-w-fit">Divizend</p>
+              <p className="overflow-hidden text-gray-200 flex-shrink-1">
+                {".".repeat(100)}
+              </p>
+              <p className="text-gray-600 min-w-fit">
+                Product Designer & Frontend Engineer
+              </p>
+            </div>
+            <p className="text-slate-400 font-jetbrains w-52">2018 — 20</p>
+          </div>
+
+          <div className="flex flex-row justify-start align-baseline gap-6 px-2 py-2 w-full">
+            <div className="flex flex-row justify-start align-baseline gap-8 w-full">
+              <p className="text-gray-800 font-medium min-w-fit">
+                CDF Technologies
+              </p>
+              <p className="overflow-hidden text-gray-200 flex-shrink-1">
+                {".".repeat(100)}
+              </p>
+              <p className="text-gray-600 min-w-fit">Co-Founder</p>
+            </div>
+            <p className="text-slate-400 font-jetbrains w-52">2018 — 20</p>
+          </div>
+
+          <div className="flex flex-row justify-start align-baseline gap-6 px-2 py-2 w-full">
+            <div className="flex flex-row justify-start align-baseline gap-8 w-full">
+              <p className="text-gray-800 font-medium min-w-fit">Audi</p>
+              <p className="overflow-hidden text-gray-200 flex-shrink-1">
+                {".".repeat(100)}
+              </p>
+              <p className="text-gray-600 min-w-fit">
+                Powertrain Simulation Engineer
+              </p>
+            </div>
+            <p className="text-slate-400 font-jetbrains w-52">2015 — 18</p>
+          </div>
+
+          <p className="bg-gradient-to-r from-amber-400 via-red-500 to-pink-400 rounded-full px-2 py-1 text-xs text-white tracking-wide mb-6  mt-20">
+            PERSONAL
           </p>
-        </a>
-      </div>
-    </main>
-  )
+          <div className="flex flex-row justify-start align-baseline gap-6 px-2 py-2 w-full cursor-pointer">
+            <a
+              href="https://homebaristacollective.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-full"
+            >
+              <div className="flex flex-row justify-start align-baseline gap-8 w-full">
+                <Image
+                  src="/wonky_small.svg"
+                  width={30}
+                  height={30}
+                  alt="Logo for Home Barista Collective"
+                  className="-mt-1"
+                />
+                <p className="text-gray-800 font-medium min-w-fit">
+                  Home Barista Collective
+                </p>
+                <p className="overflow-hidden text-gray-200 flex-shrink-1">
+                  {".".repeat(300)}
+                </p>
+                <p className="text-slate-400 font-jetbrains w-52">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2.5}
+                    stroke="currentColor"
+                    className="w-4 h-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+                    />
+                  </svg>
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
